8b3cdf46510ed63054d3855469921bcec98f839f
EvinSoftware <Novax100pre@gmail.com>
2025-04-25 17:01:41 -0500

    cambio nombre en lista productos

--- resources/views/producto/index.blade.php
+++ resources/views/producto/index.blade.php
@@ -47,7 +47,7 @@
                 <div class="col-12">
                     <div class="card">
                         <div class="card-header">
-                            <h3 class="card-title">Producto</h3>
+                            <h3 class="card-title">lista de  Producto</h3>
                         </div>
                         <div class="card-body border-bottom py-3">
                             <div class="d-flex">
